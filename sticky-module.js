77db00f523bb14a0e4864d96793bef7d6666d967
aaisara12 <aisara121201@gmail.com>
2022-06-01 17:19:09 -0700

    Added StickyModule
    The idea is that all stickiness functionality is added on top of the already existing code and can be easily extracted from the simulation. A downside is that now you have to remember to update the module in addition to the actual simulation.

---
+++ sticky-module.js
@@ -0,0 +1,40 @@
+import { Spring } from "./part_two_spring.js";
+export class StickyModule
+{
+    constructor(sim)
+    {
+        this.projectiles = [];
+        this.particles_stuck_to = [];
+        this.springs = [];
+        this.simulation = sim;
+    }
+
+    add_projectile(proj)
+    {
+        this.simulation.particles.push(proj);
+        this.projectiles.push(proj);
+    }
+
+    modify_sim()
+    {
+        this.simulation.particles.forEach((p) => 
+        {
+            this.projectiles.forEach((proj) => 
+            {
+                // If it's not the ball itself nor a particle the ball is already stuck to
+                if(proj !== p && !this.particles_stuck_to.includes(p))
+                {
+                    const stickDist = 0.5;
+                    if(proj.pos.minus(p.pos).norm() < stickDist)
+                    {
+                        const stickySpring = new Spring();
+                        stickySpring.set_spring(p, proj, 1, 1, 1);
+                        this.simulation.springs.push(stickySpring);
+                        this.particles_stuck_to.push(p);
+                    }
+                }
+            })
+            
+        })
+    }
+}
